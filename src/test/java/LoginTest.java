1b69cacd24a4d883ac8ef1337bbaa15e524457a7
Mahmoud Sami <mahmoudsamisenna@gmail.com>
2024-10-11 15:53:22 +0300

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,9 @@
+import pages.HomePage;
+import pages.LoginPage;
+import org.testng.annotations.*;
+
+public class Test extends BaseTest{
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+}
